5a5aa3e88ba270a7c2b0491d6b48e1a34778db2e
Hamzah Al-Hagi <ha.773774142@gmail.com>
2023-02-11 23:10:35 +0300

    Init the header

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
+let myHeader = document.createElement("header");
+let myNavbar = document.createElement("nav");
+let myUl = document.createElement("ul");
+let mylist = document.createElement("li");
+let myLink = document.createElement("a");
 let myProduct = document.createElement("div");
+
+myNavbar.style.display ="flex";
+myNavbar.style.justifyContent ="center";
+let linkText = document.createTextNode("Home");
+myLink.style.textDecoration = "none";
+myUl.style.listStyle = "none";
+myUl.style.display = "flex";
+myLink.appendChild(linkText);
+mylist.appendChild(myLink);
+myUl.appendChild(mylist);
+myNavbar.appendChild(myUl);
+myHeader.appendChild(myNavbar);
+
 // console.log(myProduct);
+myHeader.setAttribute('class','header');
 myProduct.setAttribute('class','product');
+// myALL.setAttribute('class',`*`);
+
 // console.log(myProduct);
-myProduct.style.cssText = ` background-color: #eee;
+document.body.style.cssText = `
+box-sizing: border-box;
+padding: 0;
+margin: 0;
+`; 
+myHeader.style.cssText = `
+width: 100%;
+height: 60px;
+background-color: #94a3b8;
+box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+margin-bottom: 8px;
+`;
+myProduct.style.cssText = `background-color: #eee;
 margin: auto;
 width: 200px;
 border-radius: 2px;
@@ -22,6 +55,6 @@ productDescripe.appendChild(descText);
 myProduct.appendChild(productTitle);
 myProduct.appendChild(productDescripe);
 // console.log(myProduct);
-
+document.body.appendChild(myHeader);
 document.body.appendChild(myProduct);
 
